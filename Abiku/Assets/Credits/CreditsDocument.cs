e8f7127ddf6cadb86bd41746dcc126c3c02c6d01
Sydney Petrehn <48782717+Strehn@users.noreply.github.com>
2020-10-04 18:59:13 -0700

    Created Intro and Main Menu

---
+++ Abiku/Assets/Credits/CreditsDocument.cs
@@ -0,0 +1,21 @@
+﻿// ----- Credits -----
+
+/*
+ * ----- Assets: -----
+ * Text Mesh Pro
+ * 
+ * ----- Audio: -----
+ * https://www.gravitysound.studio/shop-products/ambient-atmosphere-pack-1
+ * https://www.zapsplat.com/
+ * 
+ * ----- Video: -----
+ * https://invideo.io/
+ * 
+ * ----- Images: -----
+ * https://pixabay.com/photos/forest-fog-trees-aesthetic-weird-3394066/
+ * 
+ * ----- Tutorials I Followed: -----
+ * Creating a Start Menu in Unity: www.youtube.com/watch?v=zc8ac_qUXQY
+ * Implementing the Splash Intro Scene: www.youtube.com/watch?v=2kLQ8gq9xeQ and answers.unity.com/questions/350721/c-yield-waitforseconds.html
+ * Adding Background Audio: www.youtube.com/watch?v=BKCsH8mQ-IM
+ */
---
+++ Abiku/Assets/Scripts/Wait4Intro.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Wait4Intro : MonoBehaviour
+{
+    public float wait_time = 5f;
+
+    void Start()
+    {
+        StartCoroutine(WaitTime());
+        
+    }
+    IEnumerator WaitTime()
+    {
+        //Debug.Log("Waiting for Intro");
+        yield return new WaitForSeconds(wait_time);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+
+
+}
